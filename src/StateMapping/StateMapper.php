8eccc9749211ccd6c6b6541b0bef525f1862ba26
Claudio Zizza <zizza@flagbit.de>
2020-12-10 16:52:53 +0100

    Handle empty state-machine in Shopware state

--- src/StateMapping/StateMapper.php
+++ src/StateMapping/StateMapper.php
@@ -2,6 +2,9 @@
 
 namespace EinsUndEins\PluginTransactionMailExtender\StateMapping;
 
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\System\StateMachine\Aggregation\StateMachineState\StateMachineStateEntity;
 use Shopware\Core\System\SystemConfig\SystemConfigService;
 
@@ -13,12 +16,18 @@ class StateMapper implements Mapper
     private $configService;
 
     /**
+     * @var EntityRepositoryInterface
+     */
+    private $repository;
+
+    /**
      * @var string
      */
     private static $defaultState = 'OrderProcessing';
 
-    public function __construct(SystemConfigService $configService)
+    public function __construct(SystemConfigService $configService, EntityRepositoryInterface $stateMachineRepository)
     {
+        $this->repository = $stateMachineRepository;
         $this->configService = (array) ($configService->get('TransactionMailExtender.config.statusmapping') ?? []);
     }
 
@@ -33,18 +42,30 @@ class StateMapper implements Mapper
         return $this->configService[$key] ?? self::$defaultState;
     }
 
-    private function createStateMappingKey(?StateMachineStateEntity $state): string
+    private function createStateMappingKey(StateMachineStateEntity $state): string
     {
-        if ($state === null) {
-            return '';
-        }
+        $this->loadStateMachine($state);
 
         $stateMachine = $state->getStateMachine();
         $stateMachineName = '';
+
         if ($stateMachine !== null) {
             $stateMachineName = $stateMachine->getTechnicalName();
         }
 
         return $stateMachineName . '-' . $state->getTechnicalName();
+    }
+
+    private function loadStateMachine(StateMachineStateEntity $state): void
+    {
+        if ($state->getStateMachine() === null) {
+            $result = $this->repository->search(new Criteria([
+                $state->getStateMachineId()
+            ]), Context::createDefaultContext());
+
+            if (count($result) === 1) {
+                $state->setStateMachine($result->first());
+            }
+        }
     }
 }
--- tests/StateMapping/StateMapperTest.php
+++ tests/StateMapping/StateMapperTest.php
@@ -3,8 +3,10 @@
 namespace EinsUndEins\PluginTransactionMailExtender\Tests\StateMapping;
 
 use EinsUndEins\PluginTransactionMailExtender\StateMapping\StateMapper;
-use phpDocumentor\Reflection\Types\Iterable_;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\EntitySearchResult;
 use Shopware\Core\System\StateMachine\Aggregation\StateMachineState\StateMachineStateEntity;
 use Shopware\Core\System\StateMachine\StateMachineEntity;
 use Shopware\Core\System\SystemConfig\SystemConfigService;
@@ -16,6 +18,11 @@ class StateMapperTest extends TestCase
      */
     private $config;
 
+    /**
+     * @var \PHPUnit\Framework\MockObject\MockObject&EntityRepositoryInterface
+     */
+    private $repository;
+
     protected function setUp(): void
     {
         $this->config = $this->createMock(SystemConfigService::class);
@@ -26,15 +33,43 @@ class StateMapperTest extends TestCase
                 'parent.state-state1' => 'state1',
                 'parent.state-state2' => 'state2',
                 ]);
+
+        $this->repository = $this->createMock(EntityRepositoryInterface::class);
     }
 
     public function testGetValueBy(): void
     {
-        $mapping = new StateMapper($this->config);
+        $mapping = new StateMapper($this->config, $this->repository);
 
         $state = $this->createState('parent.state', 'state2');
 
         self::assertSame('state2', $mapping->getValueBy($state));
+    }
+
+    public function testGetValueByWithLoadingParentState(): void
+    {
+        $stateMachine = $this->createStub(StateMachineEntity::class);
+        $stateMachine->method('getTechnicalName')
+            ->willReturn('parent.state');
+
+        $emptyResult = $this->createMock(EntitySearchResult::class);
+        $emptyResult->expects(self::once())
+            ->method('count')
+            ->willReturn(1);
+        $emptyResult->expects(self::once())
+            ->method('first')
+            ->willReturn($stateMachine);
+
+        $this->repository->method('search')
+            ->willReturn($emptyResult);
+
+        $mapping = new StateMapper($this->config, $this->repository);
+
+        $state = new StateMachineStateEntity();
+        $state->setTechnicalName('state1');
+        $state->setStateMachineId('1234567890');
+
+        self::assertSame('state1', $mapping->getValueBy($state));
     }
 
     /**
@@ -42,7 +77,13 @@ class StateMapperTest extends TestCase
      */
     public function testDefaultIfStateNotFound(?StateMachineStateEntity $state): void
     {
-        $mapping = new StateMapper($this->config);
+        $emptyResult = $this->createStub(EntitySearchResult::class);
+        $emptyResult->method('count')->willReturn(0);
+
+        $this->repository->method('search')
+            ->willReturn($emptyResult);
+
+        $mapping = new StateMapper($this->config, $this->repository);
 
         self::assertSame('OrderProcessing', $mapping->getValueBy($state));
     }
@@ -52,14 +93,18 @@ class StateMapperTest extends TestCase
         yield 'Unknwon StateMachineStateEntity' => [$this->createState('parent.state', 'unknown')];
         yield 'No StateMachineEntity' => [$this->createState(null, 'state1')];
         yield 'null state' => [null];
-        yield 'Empty StateMachine' => [$this->createStub(StateMachineStateEntity::class)];
     }
 
+    /**
+     * @return MockObject&StateMachineStateEntity
+     */
     private function createState(?string $parentState, string $state): StateMachineStateEntity
     {
         $stateMachineState = $this->createStub(StateMachineStateEntity::class);
         $stateMachineState->method('getTechnicalName')
             ->willReturn($state);
+        $stateMachineState->method('getStateMachineId')
+            ->willReturn('1234567890');
 
         if ($parentState !== null) {
             $stateMachine = $this->createStub(StateMachineEntity::class);
